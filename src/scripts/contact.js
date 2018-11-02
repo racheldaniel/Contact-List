9cc65133a8a7a1bd06e6f6d7e8d872b647a0134b
Rachel Daniel <rachdaniel88@gmail.com>
2018-11-02 15:25:32 -0500

    refactored using jQuery

--- src/scripts/contact.js
+++ src/scripts/contact.js
@@ -2,14 +2,13 @@
 //component responsible for constructing DOM contact component
 
 const domPost = (contact) => {
-  const article = document.querySelector(".output")
   const section = document.createElement("section")
-  section.innerHTML = `
-    <h3 class="name">${contact.name}</h3>
-    <div class="phone">${contact.phone}</div>
-    <div class="address">${contact.address}</div>
-  `
-  article.appendChild(section)
+  $(section).html(`
+  <h3 class="name">${contact.name}</h3>
+  <div class="phone">${contact.phone}</div>
+  <div class="address">${contact.address}</div>
+`)
+  $(".output").prepend(section)
 }
 
 export default domPost
